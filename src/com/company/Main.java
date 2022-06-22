1db231861b1563a2abe36456248f083acd539bd0
RazielEL <amp91@pm.me>
2022-06-21 21:41:36 -0500

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
         for (int i=0 ;i<5 ;i++){
             System.out.println("Hello! Am I the blendzior?");
+            System.out.println("I am the blendzior!");
 
         }
     }
